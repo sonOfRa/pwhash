b6de7828cd78e7bb43e6dd31083fdd2480bb6772
Simon Levermann <simon@slevermann.de>
2018-01-26 18:24:43 +0100

    Refactor some tests

--- src/test/java/de/slevermann/pwhash/Argon2Test.java
+++ src/test/java/de/slevermann/pwhash/Argon2Test.java
@@ -4,7 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Argon2Test {
+    private static final String PASSWORD = "The Magic Words are Squeamish Ossifrage";
     private static Argon2Strategy defaultStrategy;
 
     @BeforeClass
@@ -14,16 +18,29 @@ public class Argon2Test {
 
     @Test
     public static void defaultType() {
-        String hash = defaultStrategy.hash("The Magic Words are Squeamish Ossifrage");
-        Assert.assertTrue(hash.startsWith("$argon2id"), "Password hash should start with $argon2id");
+        String hash = defaultStrategy.hash(PASSWORD);
+
+        String[] chunks = hash.split("\\$");
+        Assert.assertEquals(chunks[1], "argon2id", "Identifier should be argon2id");
+
+        String[] options = chunks[3].split(",");
+
+        Map<String, Integer> optionMap = new HashMap<>();
+        for (String option : options) {
+            String[] splitOpts = option.split("=");
+            optionMap.put(splitOpts[0], Integer.parseInt(splitOpts[1]));
+        }
+
+        Assert.assertEquals((int) optionMap.get("m"), Argon2Strategy.DEFAULT_MEMORY_COST, "Memory cost should be default");
+        Assert.assertEquals((int) optionMap.get("p"), Argon2Strategy.DEFAULT_THREADS, "Thread count should be default");
+        Assert.assertEquals((int) optionMap.get("t"), Argon2Strategy.DEFAULT_TIME_COST, "Time cost should be default");
     }
 
     @Test
     public static void defaultAuth() {
-        String password = "The Magic Words are Squeamish Ossifrage";
-        String hash = defaultStrategy.hash(password);
+        String hash = defaultStrategy.hash(PASSWORD);
 
-        Assert.assertTrue(defaultStrategy.verify(password, hash),
+        Assert.assertTrue(defaultStrategy.verify(PASSWORD, hash),
                 "Password verification should succeed with correct password");
         Assert.assertFalse(defaultStrategy.verify("wrong", hash),
                 "Password verification should fail with incorrect password");
@@ -31,17 +48,15 @@ public class Argon2Test {
 
     @Test
     public static void rehash() {
-        String password = "The Magic Words are Squeamish Ossifrage";
-        String hash = defaultStrategy.hash(password);
+        String hash = defaultStrategy.hash(PASSWORD);
 
-        Assert.assertFalse(defaultStrategy.needsRehash(password, hash),
+        Assert.assertFalse(defaultStrategy.needsRehash(PASSWORD, hash),
                 "Default hash should not require a rehash");
     }
 
     @Test
     public static void rehashWrongPassword() {
-        String password = "The Magic Words are Squeamish Ossifrage";
-        String hash = defaultStrategy.hash(password);
+        String hash = defaultStrategy.hash(PASSWORD);
 
         Assert.assertFalse(defaultStrategy.needsRehash("wrong", hash),
                 "Invalid password should not require rehash");
--- src/test/java/de/slevermann/pwhash/BCryptTest.java
+++ src/test/java/de/slevermann/pwhash/BCryptTest.java
@@ -5,25 +5,31 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class BCryptTest {
-    private static BCryptStrategy defaultStrategy;
+    private static final BCryptStrategy defaultStrategy = new BCryptStrategy();
+    private static final BCryptStrategy customWorkFactor = new BCryptStrategy(12);
+    private static final String PASSWORD = "The Magic Words are Squeamish Ossifrage";
 
-    @BeforeClass
-    public static void setUp() {
-        defaultStrategy = new BCryptStrategy();
-    }
 
     @Test
     public static void defaultType() {
-        String hash = defaultStrategy.hash("The Magic Words are Squeamish Ossifrage");
-        Assert.assertTrue(hash.startsWith("$2a"), "Password hash should start with $2a");
+        String hash = defaultStrategy.hash(PASSWORD);
+        String[] chunks = hash.split("\\$");
+        Assert.assertTrue(chunks[1].equals("2a"), "Identifier should be 2a");
+        Assert.assertTrue(chunks[2].equals("10"), "Work factor should be 10");
+    }
+
+    @Test
+    public static void customWorkFactor() {
+        String hash = customWorkFactor.hash(PASSWORD);
+        String[] chunks = hash.split("\\$");
+        Assert.assertTrue(chunks[2].equals("12"), "Work factor should be 12");
     }
 
     @Test
     public static void defaultAuth() {
-        String password = "The Magic Words are Squeamish Ossifrage";
-        String hash = defaultStrategy.hash(password);
+        String hash = defaultStrategy.hash(PASSWORD);
 
-        Assert.assertTrue(defaultStrategy.verify(password, hash),
+        Assert.assertTrue(defaultStrategy.verify(PASSWORD, hash),
                 "Password verification should succeed with correct password");
         Assert.assertFalse(defaultStrategy.verify("wrong", hash),
                 "Password verification should fail with incorrect password");
@@ -31,17 +37,15 @@ public class BCryptTest {
 
     @Test
     public static void noRehash() {
-        String password = "The Magic Words are Squeamish Ossifrage";
-        String hash = defaultStrategy.hash(password);
+        String hash = defaultStrategy.hash(PASSWORD);
 
-        Assert.assertFalse(defaultStrategy.needsRehash(password, hash),
+        Assert.assertFalse(defaultStrategy.needsRehash(PASSWORD, hash),
                 "Default hash should not require a rehash");
     }
 
     @Test
     public static void rehashWrongPassword() {
-        String password = "The Magic Words are Squeamish Ossifrage";
-        String hash = defaultStrategy.hash(password);
+        String hash = defaultStrategy.hash(PASSWORD);
 
         Assert.assertFalse(defaultStrategy.needsRehash("wrong", hash),
                 "Invalid password should not require rehash");
@@ -49,10 +53,9 @@ public class BCryptTest {
 
     @Test
     public static void rehash() {
-        String password = "The Magic Words are Squeamish Ossifrage";
-        String hash = new BCryptStrategy(12).hash(password);
+        String hash = new BCryptStrategy(12).hash(PASSWORD);
 
-        Assert.assertTrue(defaultStrategy.needsRehash(password, hash),
+        Assert.assertTrue(defaultStrategy.needsRehash(PASSWORD, hash),
                 "Bigger work factor should require rehash");
     }
 
