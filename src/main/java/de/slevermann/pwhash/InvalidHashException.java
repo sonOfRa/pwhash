65f226c5e813058ac3eb269030465507679ee975
Simon Levermann <simon-github@slevermann.de>
2018-01-27 01:51:11 +0100

    Add constructors for InvalidHashException

--- src/main/java/de/slevermann/pwhash/InvalidHashException.java
+++ src/main/java/de/slevermann/pwhash/InvalidHashException.java
@@ -1,9 +1,26 @@
 package de.slevermann.pwhash;
 
+import org.omg.CORBA.DynAnyPackage.Invalid;
+
 /**
  * Exception to be thrown when an invalid hash is given
  */
 public class InvalidHashException extends Exception {
+
+    /**
+     * Construct an exception with no further information
+     */
+    public InvalidHashException() {
+    }
+
+    /**
+     * Construct an exception with given cause and message
+     *
+     * @param message the error message
+     */
+    public InvalidHashException(String message) {
+        super(message);
+    }
 
     /**
      * Construct an exception with the given cause
@@ -13,4 +30,15 @@ public class InvalidHashException extends Exception {
     public InvalidHashException(Throwable cause) {
         super(cause);
     }
+
+    /**
+     * Construct an exception with given cause and message
+     *
+     * @param message the error message
+     * @param cause   the cause of the exception
+     */
+    public InvalidHashException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
